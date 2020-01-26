4c8c330cd9a75032e762597d0e1cde0a1e1da442
B98170067 <b98170067@gmail.com>
2020-01-26 22:10:42 +0800

    Master/Detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  selectedHero: Hero;
   heroes = HEROES;
 
   constructor() { }
@@ -20,4 +16,8 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
